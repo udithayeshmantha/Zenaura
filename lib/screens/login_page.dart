3c1b9b9fc3bc8e39f6e9b68018212151d6889b00
udithayeshmantha <udithayeshmantha@gmail.com>
2024-06-23 20:24:35 +0530

    chore: Remove unused imports and files

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:zenaura/widgets/Custom_scaffold.dart';
-import 'package:zenaura/widgets/welcomepgbuttons.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
--- lib/widgets/welcomepgbuttons.dart
+++ lib/widgets/welcomepgbuttons.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:zenaura/screens/login_page.dart';
 
