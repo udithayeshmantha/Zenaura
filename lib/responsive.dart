6e6da6e7083c4295210a34fa8daafdd960f40809
piyumi madushani <109897972+Instee@users.noreply.github.com>
2024-07-11 09:42:36 +0530

    Update responsive.dart

--- lib/responsive.dart
+++ lib/responsive.dart
@@ -13,7 +13,7 @@ class Responsive extends StatelessWidget {
   });
 
   static bool isMobile(BuildContext context) =>
-      MediaQuery.of(context).size.width < 1024;
+      MediaQuery.of(context).size.width < 640;
 
   static bool isTablet(BuildContext context) =>
       MediaQuery.of(context).size.width < 1280 &&
